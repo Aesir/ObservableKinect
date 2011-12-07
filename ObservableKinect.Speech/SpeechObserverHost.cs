34303c050450f95f7232a79d9410bd50f34de0c0
Bryan Anderson <anderbm@gmail.com>
2011-12-06 20:57:52 -0600

    Created the basic wrappers for the Kinect runtime and speech modules

---
+++ ObservableKinect.Speech/SpeechObserverHost.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using Microsoft.Research.Kinect.Audio;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+
+namespace ObservableKinect.Speech
+{
+	public class SpeechObserverHost
+		: IDisposable
+	{
+		private const string RECOGNIZER_ID = "SR_MS_en-US_Kinect_10.0";
+
+		private static readonly KinectAudioSource ourKinectSource;
+		private static readonly SpeechRecognitionEngine ourSpeechEngine;
+		private static int ourActiveHostsCounter = 0;
+		private static readonly object ourCounterLock = new object();
+
+		private readonly ISet<Grammar> myGrammars;
+
+		#region IObservable Properties
+
+		///<summary>
+		///Recognized a word or words that may be a component of multiple complete phrases.
+		///</summary>
+		public IObservable<SpeechHypothesizedEventArgs> SpeechHypothesized
+		{
+			get
+			{
+				return _SpeechHypothesized;
+			}
+		}
+		private readonly IObservable<SpeechHypothesizedEventArgs> _SpeechHypothesized;
+
+		///<summary>
+		///Receives input that matches any of its grammars.
+		///</summary>
+		public IObservable<SpeechRecognizedEventArgs> SpeechRecognized
+		{
+			get
+			{
+				return _SpeechRecognized;
+			}
+		}
+		private readonly IObservable<SpeechRecognizedEventArgs> _SpeechRecognized;
+
+		///<summary>
+		///Receives input that does not match any of its grammars.
+		///</summary>
+		public IObservable<SpeechRecognitionRejectedEventArgs> SpeechRecognitionRejected
+		{
+			get
+			{
+				return _SpeechRecognitionRejected;
+			}
+		}
+		private readonly IObservable<SpeechRecognitionRejectedEventArgs> _SpeechRecognitionRejected;
+
+		///<summary>
+		///Raised when speech can be identified.
+		///</summary>
+		public IObservable<SpeechDetectedEventArgs> SpeechDetected
+		{
+			get
+			{
+				return _SpeechDetected;
+			}
+		}
+		private readonly IObservable<SpeechDetectedEventArgs> _SpeechDetected;
+
+		#endregion IObservable Properties
+
+		static SpeechObserverHost()
+		{
+			ourSpeechEngine = new SpeechRecognitionEngine(RECOGNIZER_ID);
+			ourKinectSource = new KinectAudioSource
+			{
+				FeatureMode = true,
+				AutomaticGainControl = true,
+				SystemMode = SystemMode.OptibeamArrayOnly,
+				MicArrayMode = MicArrayMode.MicArrayAdaptiveBeam
+			};
+
+			ourSpeechEngine.SetInputToAudioStream(ourKinectSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+		}
+
+		public SpeechObserverHost(IEnumerable<string> keyWords)
+			: this(keyWords.ToArray())
+		{
+		}
+
+		public SpeechObserverHost(params string[] keyWords)
+			: this(new Grammar(new Choices(keyWords.ToArray()).ToGrammarBuilder()))
+		{
+		}
+
+		public SpeechObserverHost(params Grammar[] grammars)
+			: this((IEnumerable<Grammar>)grammars)
+		{ }
+
+		public SpeechObserverHost(IEnumerable<Grammar> grammars)
+		{
+			myGrammars = new HashSet<Grammar>(grammars);
+			foreach (var grammar in myGrammars)
+			{
+				ourSpeechEngine.LoadGrammarAsync(grammar);
+			}
+
+			lock (ourCounterLock)
+			{
+				if (ourActiveHostsCounter == 0)
+				{
+					ourSpeechEngine.RecognizeAsync(RecognizeMode.Multiple);
+				}
+				ourActiveHostsCounter++;
+			}
+
+			_SpeechHypothesized = EventFilterAndArgsSelector(Observable.FromEventPattern<SpeechHypothesizedEventArgs>(ourSpeechEngine, "SpeechHypothesized"));
+			_SpeechRecognized = EventFilterAndArgsSelector(Observable.FromEventPattern<SpeechRecognizedEventArgs>(ourSpeechEngine, "SpeechRecognized"));
+			_SpeechRecognitionRejected = EventFilterAndArgsSelector(Observable.FromEventPattern<SpeechRecognitionRejectedEventArgs>(ourSpeechEngine, "SpeechRecognitionRejected"));
+			_SpeechDetected = Observable.FromEventPattern<SpeechDetectedEventArgs>(ourSpeechEngine, "SpeechDetected").Select(ep => ep.EventArgs);
+		}
+
+		private IObservable<T> EventFilterAndArgsSelector<T>(IObservable<EventPattern<T>> xs) where T : RecognitionEventArgs
+		{
+			return xs.Where(ep => myGrammars.Contains(ep.EventArgs.Result.Grammar)).Select(ep => ep.EventArgs);
+		}
+
+		#region IDisposable
+
+		private bool disposed = false;
+
+		public void Dispose()
+		{
+			Dispose(true);
+
+			GC.SuppressFinalize(this);
+		}
+
+		// Dispose(bool disposing) executes in two distinct scenarios.
+		// If disposing equals true, the method has been called directly
+		// or indirectly by a user's code. Managed and unmanaged resources
+		// can be disposed.
+		// If disposing equals false, the method has been called by the
+		// runtime from inside the finalizer and you should not reference
+		// other objects. Only unmanaged resources can be disposed.
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					lock (ourCounterLock)
+					{
+						ourActiveHostsCounter--;
+						if (ourActiveHostsCounter == 0)
+						{
+							ourSpeechEngine.RecognizeAsyncStop();
+						}
+					}
+
+					foreach (var grammar in myGrammars)
+					{
+						ourSpeechEngine.UnloadGrammar(grammar);
+					}
+					myGrammars.Clear();
+				}
+				disposed = true;
+			}
+		}
+
+		~SpeechObserverHost()
+		{
+			Dispose(false);
+		}
+
+		#endregion IDisposable
+	}
+}
---
+++ ObservableKinect/KinectSensor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Reactive.Linq;
+using Microsoft.Research.Kinect.Nui;
+
+namespace ObservableKinect
+{
+	public class KinectSensor
+	{
+		#region Factory
+
+		private static readonly KinectSensor[] ourKinectSensors = new KinectSensor[Runtime.Kinects.Count];
+
+		public static KinectSensor Start(RuntimeOptions options, int kinectIndex = 0)
+		{
+			Contract.Requires(kinectIndex > 0 && kinectIndex < Runtime.Kinects.Count);
+
+			lock (ourKinectSensors)
+			{
+				if (ourKinectSensors[kinectIndex] != null)
+					AddRuntimeOptions(ourKinectSensors[kinectIndex], options);
+				else
+					ourKinectSensors[kinectIndex] = new KinectSensor(kinectIndex, options);
+
+				return ourKinectSensors[kinectIndex];
+			}
+		}
+
+		private static void AddRuntimeOptions(KinectSensor sensor, RuntimeOptions options)
+		{
+			sensor.myOptions |= options;
+
+			sensor.Device.Uninitialize();
+			sensor.Device.Initialize(sensor.myOptions);
+		}
+
+		#endregion Factory
+
+		private readonly int myIndex;
+		private RuntimeOptions myOptions;
+
+		protected KinectSensor(int index, RuntimeOptions options)
+		{
+			Contract.Requires(index > 0 && index < Runtime.Kinects.Count);
+
+			myIndex = index;
+			myOptions = options;
+
+			this.Device.Initialize(options);
+
+			_DepthFrames = Observable.FromEventPattern<ImageFrameReadyEventArgs>(this.Device, "DepthFrameReady").Select(ep => ep.EventArgs);
+			_SkeletonFrames = Observable.FromEventPattern<SkeletonFrameReadyEventArgs>(this.Device, "SkeletonFrameReady").Select(ep => ep.EventArgs);
+			_VideoFrames = Observable.FromEventPattern<ImageFrameReadyEventArgs>(this.Device, "VideoFrameReady").Select(ep => ep.EventArgs);
+		}
+
+		public Runtime Device
+		{
+			get { return Runtime.Kinects[myIndex]; }
+		}
+
+		public IObservable<ImageFrameReadyEventArgs> DepthFrames
+		{
+			get
+			{
+				return _DepthFrames;
+			}
+		}
+		private readonly IObservable<ImageFrameReadyEventArgs> _DepthFrames;
+
+		public bool DepthFramesRunning { get { return _DepthFramesRunning; } }
+		private bool _DepthFramesRunning;
+
+		public IObservable<SkeletonFrameReadyEventArgs> SkeletonFrames
+		{
+			get
+			{
+				return _SkeletonFrames;
+			}
+		}
+		private readonly IObservable<SkeletonFrameReadyEventArgs> _SkeletonFrames;
+
+		public bool SkeletonFramesRunning { get { return _SkeletonFramesRunning; } }
+		private bool _SkeletonFramesRunning;
+
+		public IObservable<ImageFrameReadyEventArgs> VideoFrames
+		{
+			get
+			{
+				return _VideoFrames;
+			}
+		}
+		private readonly IObservable<ImageFrameReadyEventArgs> _VideoFrames;
+
+		public bool VideoFramesRunning { get { return _VideoFramesRunning; } }
+		private bool _VideoFramesRunning;
+
+		public void StartDepthFrames(ImageResolution resolution, bool includePlayerIndex = false, int numBuffers = 2)
+		{
+			Contract.Requires(!this.DepthFramesRunning);
+			Contract.Requires(numBuffers > 0);
+
+			if (!this.DepthFramesRunning)
+			{
+				var type = includePlayerIndex ? ImageType.DepthAndPlayerIndex : ImageType.Depth;
+				this.Device.DepthStream.Open(ImageStreamType.Depth, numBuffers, resolution, type);
+				_DepthFramesRunning = true;
+			}
+		}
+
+		public void StartVideoFrames(ImageResolution resolution, ImageType type = ImageType.Color, int numBuffers = 2)
+		{
+			Contract.Requires(!this.VideoFramesRunning);
+			Contract.Requires(type == ImageType.Color || type == ImageType.ColorYuv || type == ImageType.ColorYuvRaw);
+			Contract.Requires(numBuffers > 0);
+
+			if (!this.VideoFramesRunning)
+			{
+				this.Device.VideoStream.Open(ImageStreamType.Video, numBuffers, resolution, type);
+				_VideoFramesRunning = true;
+			}
+		}
+	}
+}
